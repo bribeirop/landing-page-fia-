12135dcbf1b2033aa1d6f9cd6908c28eba594ab6
Azzy <barbararibeiropeccatto@gmail.com>
2026-01-19 14:32:21 -0300

    Create faq.js

---
+++ assets/css/faq.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.faq-question').forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('active');
+    const answer = button.nextElementSibling;
+    answer.style.display =
+      answer.style.display === 'block' ? 'none' : 'block';
+  });
+});
